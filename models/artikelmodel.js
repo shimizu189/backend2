df102fb45fe5dcd588358222761649c1ddea8517
AnoukN <anouk-esther.noll@informatik.hs-fulda.de>
2024-06-04 16:23:19 +0200

    back

--- models/artikelmodel.js
+++ models/artikelmodel.js
@@ -6,7 +6,4 @@ class Ausleihvorgang {
       this.enddatum = enddatum; // Datum, an dem der Artikel zurückgegeben werden soll
       this.ausleihartikelIds = ausleihartikelIds; // IDs der ausgeliehenen Artikel
     }
-  }jkos
-  fdfd
-  dsdsd
-  afcuigasicubgqacbaweuhcewc
+  }
